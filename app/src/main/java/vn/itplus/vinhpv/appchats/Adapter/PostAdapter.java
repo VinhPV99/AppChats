f85d231c62a58f0411ed2013741fe34a275a12f6
quangvinh <quangvinh99bg@gmail.com>
2022-05-26 10:07:29 +0700

    Delete Posts OK

--- app/src/main/java/vn/itplus/vinhpv/appchats/Adapter/PostAdapter.java
+++ app/src/main/java/vn/itplus/vinhpv/appchats/Adapter/PostAdapter.java
@@ -1,21 +1,40 @@
 package vn.itplus.vinhpv.appchats.Adapter;
 
+import static android.view.View.GONE;
+
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.text.format.DateFormat;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 import java.util.Calendar;
@@ -30,16 +49,20 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.MyHolder> {
 
     Context context;
     List<Post> postList;
+    String myUid;
+    ProgressDialog mProgressDialog;
 
     public PostAdapter(Context context, List<Post> postList) {
         this.context = context;
         this.postList = postList;
+        myUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
     @androidx.annotation.NonNull
     @Override
     public MyHolder onCreateViewHolder(@androidx.annotation.NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.row_posts, parent, false);
+        mProgressDialog = new ProgressDialog(context);
         return new MyHolder(view);
     }
 
@@ -63,8 +86,16 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.MyHolder> {
         // set data
         holder.uNameTv.setText(uName);
         holder.pTimeTv.setText(pTime);
-        holder.pTitleTv.setText(pTitle);
-        holder.pDescriptionTv.setText(pDescription);
+        if(pTitle == null){
+            holder.pTitleTv.setVisibility(GONE);
+        }else {
+            holder.pTitleTv.setText(pTitle);
+        }
+        if(pDescription == null){
+            holder.pDescriptionTv.setVisibility(GONE);
+        }else {
+            holder.pDescriptionTv.setText(pDescription);
+        }
 
         // set user dp
         try {
@@ -75,8 +106,9 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.MyHolder> {
         // if there is no image i.e. pImage.equals("no Image")then hide ImageView
         if (pImage.equals("noImage")) {
             // hide imageview
-            holder.pImageIv.setVisibility(View.GONE);
+            holder.pImageIv.setVisibility(GONE);
         } else {
+            holder.pImageIv.setVisibility(View.VISIBLE);
             try {
                 Picasso.get().load(pImage).into(holder.pImageIv);
             } catch (Exception e) {
@@ -84,10 +116,10 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.MyHolder> {
         }
         // handle button clicks,
         holder.moreBtn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
-                // will implement later
-                Toast.makeText(context, "More", Toast.LENGTH_SHORT).show();
+                showMoreOptions(holder.moreBtn, uid, myUid, pId, pImage);
             }
         });
 
@@ -119,8 +151,86 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.MyHolder> {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, ThereProfileActivity.class);
-                intent.putExtra("uid",uid);
+                intent.putExtra("uid", uid);
                 context.startActivity(intent);
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void showMoreOptions(ImageButton moreBtn, String uid, String myUid, String pId, String pImage) {
+
+        PopupMenu popupMenu = new PopupMenu(context, moreBtn, Gravity.END);
+        if (uid.equals(myUid)) {
+            popupMenu.getMenu().add(Menu.NONE, 0, 0, "Delete");
+        }
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                if (id == 0) {
+                    //delete post click
+                    deletePost(pId,pImage);
+                }
+                return false;
+            }
+        });
+        popupMenu.show();
+    }
+
+    private void deletePost(String pId, String pImage) {
+        if(pImage.equals("noImage")){
+            deleteWithoutImage(pId);
+        }else{
+            deleteWithImage(pId,pImage);
+        }
+    }
+
+    private void deleteWithImage(String pId, String pImage) {
+        mProgressDialog.setMessage("Deleting...");
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(pImage);
+        storageReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Query query = FirebaseDatabase.getInstance().getReference("Posts").orderByChild("pId").equalTo(pId);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@androidx.annotation.NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot ds : snapshot.getChildren()){
+                            ds.getRef().removeValue();
+                        }
+                        mProgressDialog.dismiss();
+                    }
+
+                    @Override
+                    public void onCancelled(@androidx.annotation.NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@androidx.annotation.NonNull Exception e) {
+
+            }
+        });
+    }
+
+    private void deleteWithoutImage(String pId) {
+        Query query = FirebaseDatabase.getInstance().getReference("Posts").orderByChild("pId").equalTo(pId);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@androidx.annotation.NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    ds.getRef().removeValue();
+                }
+                mProgressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@androidx.annotation.NonNull DatabaseError error) {
+
             }
         });
     }
@@ -138,6 +248,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.MyHolder> {
         ImageButton moreBtn;
         Button likeBtn, commentBtn, shareBtn;
         LinearLayout profileLayout;
+
         public MyHolder(@NonNull View itemView) {
             super(itemView);
             // init views
